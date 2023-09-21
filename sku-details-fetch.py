50fb953d33923117e207122b320af8427ad8a9f2
sszdhlh <77661407+sszdhlh@users.noreply.github.com>
2023-09-21 15:30:46 +1000

    Finally version for sku-details-fetch
    finally version for details -fetch and calculate the final results

--- sku-details-fetch.py
+++ sku-details-fetch.py
@@ -4,7 +4,7 @@ import openpyxl
 template_wb = openpyxl.load_workbook('template.xlsx')
 rasson_wb = openpyxl.load_workbook('work.xlsx')
 
-template_ws = template_wb.active
+template_ws = template_wb["SKUs"]
 rasson_ws = rasson_wb.active
 
 # 获取work.xlsx中的必要列索引
@@ -20,38 +20,44 @@ for row in range(2, rasson_ws.max_row + 1):
     packing_size_raw = rasson_ws.cell(row=row, column=packing_size_col).value
     weight = rasson_ws.cell(row=row, column=weight_col).value
 
-    # 检查 packing_size_raw 是否有效
-    if packing_size_raw is None or not isinstance(packing_size_raw, str):
-        continue
-
     if sku not in sku_details:
-        sku_details[sku] = []
+        sku_details[sku] = {"weight": [], "length": [], "width": [], "height": []}
 
     try:
         packing_size = packing_size_raw.replace('m', '').split('=')[0].strip()
-        lengths = packing_size.split('*')
-        if len(lengths) != 3:
-            raise ValueError(f"Expected 3 values separated by *, but got {len(lengths)} in {packing_size}")
-
-        length, width, height = map(float, lengths)
-        sku_details[sku].append((weight, length*100, width*100, height*100))
+        length, width, height = map(float, packing_size.split('*'))
+        sku_details[sku]["weight"].append(weight)
+        sku_details[sku]["length"].append(length*100)
+        sku_details[sku]["width"].append(width*100)
+        sku_details[sku]["height"].append(height*100)
     except Exception as e:
         print(f"在第{row}行遇到了问题: {e}")
 
-# 更新template.xlsx中的数据
+# 更新"SKUs"工作表中的数据
 for row in range(2, template_ws.max_row + 1):
     sku = template_ws.cell(row=row, column=1).value
-    details_list = sku_details.get(sku, [])
-
-    if details_list:
-        for idx, (weight, length, width, height) in enumerate(details_list, start=1):
+    details = sku_details.get(sku, None)
+    if details:
+        for idx, (weight, length, width, height) in enumerate(zip(details["weight"], details["length"], details["width"], details["height"]), start=1):
             template_ws.cell(row=row, column=1 + (idx-1)*5 + 1).value = weight
             template_ws.cell(row=row, column=1 + (idx-1)*5 + 2).value = length
             template_ws.cell(row=row, column=1 + (idx-1)*5 + 3).value = width
             template_ws.cell(row=row, column=1 + (idx-1)*5 + 4).value = height
             template_ws.cell(row=row, column=1 + (idx-1)*5 + 5).value = 1
-    else:
-        print(f"没有找到SKU {sku} 对应的数据")
+
+# 更新"Total"工作表的数据
+total_ws = template_wb["Total"]
+current_row = 2
+for sku, details in sku_details.items():
+    total_weight = sum(details["weight"])
+    max_length = max(details["length"])
+    max_width = max(details["width"])
+    total_height = sum(details["height"])
+    total_ws.cell(row=current_row, column=1).value = sku
+    total_ws.cell(row=current_row, column=2).value = total_weight
+    total_ws.cell(row=current_row, column=3).value = max_length
+    total_ws.cell(row=current_row, column=4).value = max_width
+    total_ws.cell(row=current_row, column=5).value = total_height
+    current_row += 1
 
 template_wb.save('processed_template.xlsx')
-
